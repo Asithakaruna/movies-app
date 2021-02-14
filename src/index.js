9efd20e6e32f5cf3996c855409919c6575509559
AsithaKaruna <asithak08@gmail.com>
2021-02-14 19:39:47 +0530

    Added ‘Home

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Home from './screens/home/Home'
 
-ReactDOM.render(<div></div>,document.getElementById('root'));
+ReactDOM.render(<Home />,document.getElementById('root'));
 reportWebVitals();
