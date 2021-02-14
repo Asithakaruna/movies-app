5deace33194f3aa4cd3967a3272a433424136715
AsithaKaruna <asithak08@gmail.com>
2021-02-14 19:31:50 +0530

    AddmissingFiles

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(<div></div>,document.getElementById('root'));
+reportWebVitals();
